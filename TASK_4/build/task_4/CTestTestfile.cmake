72038d38fcfb94ebcc2ccc930a460f997966601e
Alexfrom38 <sasha.rt20@yandex.ru>
2026-04-09 15:58:23 +0000

    build was deleted

--- TASK_4/build/task_4/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /robots_HW/TASK_4/my_robot_pkg
-# Build directory: /robots_HW/TASK_4/build/task_4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
