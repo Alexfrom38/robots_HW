107067364e98249232cb62a15fe8753e0a859998
Alexfrom38 <sasha.rt20@yandex.ru>
2026-05-08 23:30:13 +0000

    task 7 was added

---
+++ TASK_7/gazebo_controller_pkg/build/gazebo_controller_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /robots_HW/TASK_7/gazebo_controller_pkg
+# Build directory: /robots_HW/TASK_7/gazebo_controller_pkg/build/gazebo_controller_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
